c8d348a5a48b9a3bfea93ed8460455dd798839c7
carlosmfps <carlosmorvan1999@gmail.com>
2025-10-21 22:15:07 -0300

    .

---
+++ stock-buy-and-sell/stocks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void maxProfit(int arr[], int N)
+{
+}
+
+int main()
+{
+  int prices[] = {100, 180, 260, 310, 40, 535, 695};
+
+  int N = sizeof(arr) / sizeof(arr[0]);
+
+  maxProfit(prices, N);
+}
